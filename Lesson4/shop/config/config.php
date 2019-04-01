df100a103884f53f1172c2c81f32cedb16feaaf2
admin <admin@poginter.ru>
2019-04-01 23:03:42 +0300

    add config

---
+++ Lesson4/shop/config/config.php
@@ -0,0 +1,10 @@
+<?
+define("MYSQL_SERVER","localhost");
+define("MYSQL_LOGIN","root");
+define("MYSQL_PASSWORD","");
+define("MYSQL_DB","catalog");
+
+define("DIR_BIG","img/");
+define("DIR_SMALL","imgMini/");
+
+define("COLS",3);
