c5d433340ff3dcfeacb980f3ee410d0cc79b3ea4
admin <admin@poginter.ru>
2019-04-05 16:29:35 +0300

    create controller, views and models for user autorization and registration

--- Lesson5/c/C_User.php
+++ Lesson5/c/C_User.php
@@ -8,9 +8,15 @@
 
 class C_User extends C_Base
 {
+    public function action_index(){
+        $this->title .= '::Авторизация';
+//        $text = Text::text_get();
+        $this->content = $this->Template('v/v_user_index.php', array('text' => $text));
+    }
+
     public function action_login(){
         $this->title .= '::Авторизация';
-        $text = Text::text_get();
-        $this->content = $this->Template('v/v_index.php', array('text' => $text));
+//        $text = Text::text_get();
+        $this->content = $this->Template('v/v_user_index.php', array('text' => $text));
     }
 }
---
+++ Lesson5/m/User.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 05.04.2019
+ * Time: 16:01
+ */
+
+class User
+{
+    protected $loginStatus;
+    protected $userName;
+    protected $userId;
+
+    public function getLoginStatus()
+    {
+        return $this->loginStatus;
+    }
+
+    public function setLoginStatus($status)
+    {
+        $this->loginStatus = $status;
+    }
+
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    public function setUserName($userName)
+    {
+        $this->userName = $userName;
+    }
+
+    public function login($username, $password)
+    {
+        $users = include __DIR__ . '/data/users.php';
+
+        foreach ($users as $user) {
+            if ($user['username'] == $username && $user['password'] == $password) {
+                $this->setLoginStatus(true);
+                $this->setUserName($username);
+                $this->setUserId($user['id']);
+                return true;
+            } else return false;
+        }
+    }
+
+    public function logout()
+    {
+        $this->setUserId(null);
+        $this->setUserName(null);
+        $this->setLoginStatus(false);
+        session_destroy();
+    }
+
+}
